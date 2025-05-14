97f1cd3d7e31d47bce934ca9e2b5df43a0695a5d
gabzzola <gazzola.co@gmail.com>
2025-05-13 23:33:39 -0300

    style: criar o footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,41 @@
-import Container from '../layouts/Container';
+import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
+import styles from './Footer.module.css';
 
 function Footer() {
+  const icons = [
+    {
+      icon: <FaEnvelope />,
+      href: "mailto:gabzzola@gmail.com",
+      style: "email"
+    },
+    {
+      icon: <FaGithub />,
+      href: "https://github.com/gabzzola",
+      style: "github"
+    },
+    {
+      icon: <FaLinkedin />,
+      href: "https://www.linkedin.com/in/gabriel-gazzola-61a051307/",
+      style: "linkedin"
+    }
+  ];
+
   return (
     <footer>
-      <Container>
-        Footer
-      </Container>
+      <ul className={styles.list}>
+        {
+          icons.map((item, index) => (
+            <li key={index} className={styles[item.style]}>
+              <a href={item.href} target="_blank" rel="noopener noreferrer">
+                {item.icon}
+              </a>
+            </li>
+          ))
+        }
+      </ul>
+      <div>
+        <h4 className={styles.title}>Gerenciador de Projetos</h4>
+      </div>
     </footer>
   );
 }
